43a5d7fa02137c6532aab2217747cacdd4a98ee4
Henry Finucane <henry.finucane@locationlabs.com>
2014-08-26 15:09:08 -0700

    Add Hello World action plugin

---
+++ action_plugins/hello.py
@@ -0,0 +1,11 @@
+from ansible.runner.return_data import ReturnData
+
+
+class ActionModule(object):
+    def __init__(self, runner):
+        self.runner = runner
+
+    def run(self, conn, tmp, module_name, module_args, inject, complex_args=None, **kwargs):
+        return ReturnData(conn=conn,
+                          comm_ok=True,
+                          result=dict(failed=False, changed=False, msg="Hello World"))
